2a373391fa423af741998d0b9d5dab5770426231
project <xsam222m@gmail.com>
2026-03-02 02:26:49 +0200

    edited the home screen and made whenever u click on a tab it shows the body of the tab

--- lib/tabs/profile/profile_tab.dart
+++ lib/tabs/profile/profile_tab.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:movies/utitities/app_colors.dart';
 
 class ProfileTab extends StatelessWidget {
   const ProfileTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.black,
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Column(
+                children: [
+                  Container(
+
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
